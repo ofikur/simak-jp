e2f6426e8d3bb917eaff4617a1008a8c12ea8d69
Ofikur R. <135510163+ofikur@users.noreply.github.com>
2025-07-31 21:20:21 +0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUoVoKglr0nYHkwZ6oIDf13lO9Zki0vnw",
-  authDomain: "simpelkas-70ed9.firebaseapp.com",
-  projectId: "simpelkas-70ed9",
-  storageBucket: "simpelkas-70ed9.firebasestorage.app",
-  messagingSenderId: "719170749400",
-  appId: "1:719170749400:web:1fe1c4918922cbcd47e8ad"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
